893d7aa62c55985040c8f684ba3aef7565e9cffc
madx <root@yapok.org>
2008-10-30 14:48:33 +0100

    adding quotes

--- shumblr.rb
+++ shumblr.rb
@@ -12,11 +12,12 @@ Shoes.app :title => "Shumblr.", :width => 700, :height => 600 do
   background '#334668'
   background 'img/background.png', :height => 501, :width => 1.0
 
-  def home
+  def home(&blk)
     @contents.clear do
       para strong("Welcome to Shumblr!\n"),
         "Shumblr is a client for Tumblr.\nYou can post to your Tumblr from ",
         "here without having to open your browser.\n"
+      yield if block_given?
     end
   end
 
@@ -39,10 +40,10 @@ Shoes.app :title => "Shumblr.", :width => 700, :height => 600 do
     background rgb(51, 70, 104, 0.5), :curve => 12
     flow :margin => [80, 10, 90, 10] do
       background white, :curve => 5
-      image 'img/text.png',  :click => proc { check { new_text } }
+      image 'img/text.png',  :click => proc { check { new_text  } }
       image 'img/photo.png', :click => proc { check { } }
-      image 'img/quote.png', :click => proc { check { } }
-      image 'img/link.png',  :click => proc { check { new_link } }
+      image 'img/quote.png', :click => proc { check { new_quote } }
+      image 'img/link.png',  :click => proc { check { new_link  } }
       image 'img/chat.png',  :click => proc { check { } }
       image 'img/audio.png', :click => proc { check { } }
       image 'img/video.png', :click => proc { check { } }
@@ -57,7 +58,7 @@ Shoes.app :title => "Shumblr.", :width => 700, :height => 600 do
   def new_text
     @contents.clear do
 
-      title "Title (optional)"
+      title "Title ", em("(optional)")
       @title = edit_line :width => 1.0, :margin => [10, 5, 10, 10]
 
       title "Text"
@@ -84,10 +85,10 @@ Shoes.app :title => "Shumblr.", :width => 700, :height => 600 do
       title "URL"
       @url = edit_line :width => 1.0, :margin => [10, 5, 10, 10]
 
-      title "Name (optional)"
+      title "Name ", em("(optional)")
       @name = edit_line :width => 1.0, :margin => [10, 5, 10, 10]
 
-      title "Description (optional)"
+      title "Description ", em("(optional)")
       @desc = edit_box :width => 1.0, :margin => [10, 5, 10, 10]
 
       @controls = flow do
@@ -107,6 +108,30 @@ Shoes.app :title => "Shumblr.", :width => 700, :height => 600 do
     end
   end
 
+  def new_quote
+    @contents.clear do
+
+      title "Text"
+      @text = edit_box :width => 1.0, :margin => [10, 5, 10, 10]
+
+      title "Source ", em("(optional)")
+      @source = edit_line :width => 1.0, :margin => [10, 5, 10, 10]
+
+      @controls = flow do
+        button "Post" do
+          text   = @text.text
+          source = @source.text.empty? ? nil : @source.text
+          if text.empty?
+            alert "No text provided"
+          else
+            write_to_api { quote(text, source) }
+          end
+        end
+        button("Cancel") { home }
+      end
+    end
+  end
+
   def check(&blk)
     if @logged then yield else alert "Please login first" end
   end
@@ -114,7 +139,7 @@ Shoes.app :title => "Shumblr.", :width => 700, :height => 600 do
   def write_to_api(&blk)
     begin
       Tumblr::API.write($mail.text, $pass.text, "Shumblr", &blk)
-      home
+      home { para "Content published!", :stroke => green, :font => 'bold' }
     rescue Tumblr::API::AuthError
       alert "Authentication error"
       @credentials.show
